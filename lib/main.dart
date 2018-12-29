5121f028c7a37124fa6382471e8069f1504c44ad
da <da@das-iMac.local>
2018-12-29 16:31:55 +0800

    movie

--- lib/main.dart
+++ lib/main.dart
@@ -34,12 +34,12 @@ class _MyhomeState  extends State<Myhome>{
                 accountName: Text("ab"),
                 accountEmail: Text("abc@inst.cn"),
                 currentAccountPicture: CircleAvatar(
-                  backgroundImage: NetworkImage('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546008629750&di=5c4d1d4b6178c63eb5838d7fe4723ce0&imgtype=0&src=http%3A%2F%2Fimg4.duitang.com%2Fuploads%2Fitem%2F201603%2F08%2F20160308194407_24Prs.thumb.700_0.png'),
+                  backgroundImage: NetworkImage('https://fsb.nn888.xyz/111.jpg'),
                 ),
                 decoration: BoxDecoration(
                   image: DecorationImage(
                     fit: BoxFit.cover,
-                    image: NetworkImage("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546008797868&di=e190370ab3bdd7215b893a60a19e73ee&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201503%2F18%2F20150318173303_kVrhA.jpeg"))
+                    image: NetworkImage("https://fsb.nn888.xyz/222.jpg"))
 
                 ),
 
